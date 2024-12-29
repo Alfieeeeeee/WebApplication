9a1c0265d1d0d33a47a3b2e298e23986f0ee27f7
Alfie <AdobeOnly.Alfie@gmail.com>
2024-12-30 04:00:41 +0800

    新增model

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace MyWebApplication.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
